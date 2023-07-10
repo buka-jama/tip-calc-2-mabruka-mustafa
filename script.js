6d6ff6ca953bd5a3b2ae56c595e393630669e7a9
unknown <mabrukamustafam@gmail.com>
2023-07-10 22:23:03 +0300

    Answered

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello");
+function calcTip(bill) {
+    let tip;
+    if (bill >= 50 && bill <= 300) {
+        tip = bill * 0.15;
+    } else {
+        tip = bill * 0.2;
+    }
+    return tip;
+}
+
+
+console.log("For a bill of 100, the tip is " + calcTip(100));
+console.log("For a bill of 500, the tip is " + calcTip(500));
+console.log("For a bill of 30, the tip is " + calcTip(30));
+
+let tip1 = calcTip(100);
+let tip2 = calcTip(500);
+let tip3 = calcTip(30);
+
